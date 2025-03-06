709cc570069186bbe65e53eeffc16ba3ae98a148
Thorbjørn Larsen <thla19ac@student.cbs.dk>
2025-03-06 13:49:39 +0100

    Reorganising the repo

--- Simulation/Short_rate_simulation.py
+++ Simulation/Short_rate_simulation.py
@@ -1,9 +1,14 @@
+import os
+import subprocess
 import pandas as pd
 from Models.vasicek_model import VasicekModelExtended
 
 def main():
-    data_file = r"C:\Users\thorb\Documents\Github Repositories\AEF_msc_thesis_GBI\Simulation\Data\short_rate.xlsx"
-    model = VasicekModelExtended(data_file,220)
+    root = subprocess.run(["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True).stdout.strip()
+    data_folder = os.path.join(root, "Archive", "Data")
+
+    data = os.path.join(data_folder, "short_rate.xlsx")
+    model = VasicekModelExtended(data,220)
     r0=model.rates.iloc[-1,0]
     simulated_short_rates=model.run_model(
         r0,
@@ -18,3 +23,10 @@ def main():
 
 if __name__ == "__main__":
     simulated_short_rates=main()
+
+"""
+Denmark Short-Term Rate (DESTR) is a transaction-based reference rate based on unsecured overnight deposit transactions.
+Danmarks Nationalbank calculates DESTR on all Danish banking days based on the specific transactions made in the
+Danish krone market the previous banking day. Reference rates are used in a wide range of financial contracts,
+including bank loans, mortgage bonds and interest rate swaps.
+"""
