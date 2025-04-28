b4175a439b4748bf205bccb98e8f82b8b9f29b13
Thorbjørn Larsen <thla19ac@student.cbs.dk>
2025-04-28 13:31:39 +0200

    Bond returns

--- codelib/sampling/data_sampling.py
+++ codelib/sampling/data_sampling.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 def yearly_price(file, ppa=52, header=True):
+    """
+    If when loading an excel file to a dataframe, the heading is recognized (i.e. you don't include header=None), then header should be False here
+    """
     if header == True:
         yearly_columns = file.iloc[1:, ::ppa]
 
